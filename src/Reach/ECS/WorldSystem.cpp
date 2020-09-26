2eec990162beb04a8ccbcd663d4af37fc71bfadc
compiler <lukerochedev@gmail.com>
2020-09-26 19:27:09 -0400

    axis collisions

--- src/Reach/ECS/WorldSystem.cpp
+++ src/Reach/ECS/WorldSystem.cpp
@@ -39,9 +39,21 @@ void reach::WorldSystem::update(entt::basic_registry<entt::entity>* registry){
             //begin doing things with segments
             for(int i = 0; i < world.spacialEntities.size(); i++){
                 auto& segment = world.spacialEntities[i].entities;
-                if(segment.size() == 2) REACH_WARN("2 entities in same segment");
-                if(segment.size() == 3) REACH_WARN("3 entities in same segment");
-                if(segment.size() == 4) REACH_WARN("4 entities in same segment");
+                if(segment.size() >= 2){
+                    for(int k = 1; k < segment.size(); k++){
+                        auto bodyA = segment[k-1];
+                        auto bodyB = segment[k];
+                        auto collidables = registry->view<TransformComponent, CollidableComponent>();
+                        auto& bodyATrans = collidables.get<TransformComponent>(bodyA); 
+                        auto& bodyBTrans = collidables.get<TransformComponent>(bodyB); 
+                        glm::vec2 bodyACenter = bodyATrans.position + (bodyATrans.scale / 2.0f);
+                        glm::vec2 bodyBCenter = bodyBTrans.position + (bodyBTrans.scale / 2.0f);
+                        if(bodyATrans.position.x >= bodyBTrans.position.x && bodyATrans.position.x <= bodyBTrans.position.x + bodyBTrans.scale.x){
+                            REACH_WARN("Collision");
+                        }
+                    }
+
+                }
             }
         }
 
