8c4ccf75d9a315ab95d666c60cf8ef354c12fd7f
compiler <lukerochedev@gmail.com>
2020-10-10 22:47:03 -0400

    resetting collision stuff

--- src/Reach/Collision/CollisionSystem.cpp
+++ src/Reach/Collision/CollisionSystem.cpp
@@ -5,19 +5,8 @@ void reach::CollisionSystem::update(entt::basic_registry<entt::entity>* registry
     auto group = registry->view<TransformComponent, CollidableComponent>();
     for(auto en: group){
         auto&&[transform, physics] = group.get<TransformComponent, PhysicsComponent>(en);
-        getProjectedPosition(registry, transform.position);
+        
     }   
-
-
-}
-
-void reach::CollisionSystem::getProjectedPosition(entt::basic_registry<entt::entity>* registry, glm::vec2& placeToGo){
-    auto group = registry->view<TransformComponent, CollidableComponent>();
-    for(auto entity: group) {
-        auto&&[transform, collidable] = group.get<TransformComponent, CollidableComponent>(entity);
-        if(collidable.x)
-        //transform.position += physics.velocity;
-    }
 
 
 }
